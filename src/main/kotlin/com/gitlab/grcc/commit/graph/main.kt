4caec137b25d30e7c03890c8afa6bfe5044be127
sya-ri <sya79lua@gmail.com>
2020-10-16 21:07:39 +0900

    プロジェクトの取得とグループの取得を分けた

--- src/main/kotlin/com/gitlab/grcc/commit/graph/main.kt
+++ src/main/kotlin/com/gitlab/grcc/commit/graph/main.kt
@@ -12,28 +12,44 @@ suspend fun main() {
     val groupId = readLine() ?: return
     val client = GitLabApiClient(accessToken)
     println()
+    print("Getting All Groups ... ")
+    val groups = client.getAllGroup(groupId.slashTo2F)
+    println(groups.size)
+    println()
     print("Getting All Projects ... ")
-    val projects = client.getAllProject(groupId.slashTo2F)
+    val projects = client.getAllProject(groups)
     println(projects.size)
     println()
+    println(groups)
     println(projects)
+}
+
+data class Group(val id: String)
+
+@ExperimentalStdlibApi
+suspend fun GitLabApiClient.getAllGroup(groupId: String): Set<Group> {
+    return buildSet {
+        add(Group(groupId))
+        val subGroupJson = request(ApiEndPoint.GetSubGroup(groupId)) ?: return@buildSet
+        subGroupJson.asJsonArray.forEach {
+            addAll(getAllGroup(groupId + "%2F" + it.asJsonObject["path"].asString))
+        }
+    }
 }
 
 data class Project(val name: String, val groupId: String)
 
 @ExperimentalStdlibApi
-suspend fun GitLabApiClient.getAllProject(groupId: String): Set<Project> {
+suspend fun GitLabApiClient.getAllProject(groups: Set<Group>): Set<Project> {
     return buildSet {
-        val subGroupJson = request(ApiEndPoint.GetSubGroup(groupId)) ?: return@buildSet
-        subGroupJson.asJsonArray.forEach {
-            addAll(getAllProject(groupId + "%2F" + it.asJsonObject["path"].asString))
-        }
-        val projectJson = request(ApiEndPoint.GetProject(groupId)) ?: return@buildSet
-        projectJson.asJsonArray.forEach {
-            val jsonObject = it.asJsonObject
-            val name = jsonObject["name"].asString
-            val projectGroupId = jsonObject["path"].asString
-            add(Project(name, projectGroupId))
+        groups.forEach { group ->
+            val projectJson = request(ApiEndPoint.GetProject(group.id)) ?: return@buildSet
+            projectJson.asJsonArray.forEach {
+                val jsonObject = it.asJsonObject
+                val name = jsonObject["name"].asString
+                val projectGroupId = jsonObject["path"].asString
+                add(Project(name, projectGroupId))
+            }
         }
     }
 }

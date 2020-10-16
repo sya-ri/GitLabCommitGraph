ec90b0729a0069e302a9a81b1112d4d921739cc9
sya-ri <sya79lua@gmail.com>
2020-10-16 21:12:47 +0900

    説明コメントを追加

--- src/main/kotlin/com/gitlab/grcc/commit/graph/main.kt
+++ src/main/kotlin/com/gitlab/grcc/commit/graph/main.kt
@@ -7,21 +7,32 @@ import com.gitlab.grcc.commit.graph.http.GitLabApiClient
 
 @ExperimentalStdlibApi
 suspend fun main() {
+    // アクセストークンを入力
     print("AccessToken: ")
     val accessToken = readLine()
     if (accessToken.isNullOrBlank()) return
+
+    // 取得するトップグループを入力
     print("GroupId: ")
     val groupId = readLine()
     if (groupId.isNullOrBlank()) return
+
+    // ApiClient を定義
     val client = GitLabApiClient(accessToken)
+
+    // グループの取得
     println()
     print("Getting All Groups ... ")
     val groups = client.getAllGroup(groupId.slashTo2F)
     println(groups.size)
+
+    // プロジェクトの取得
     println()
     print("Getting All Projects ... ")
     val projects = client.getAllProject(groups)
     println(projects.size)
+
+    // デバッグメッセージ
     println()
     println(groups)
     println(projects)

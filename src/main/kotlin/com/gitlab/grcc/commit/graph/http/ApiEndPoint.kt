93d5799627e36ee3c4f40c0b30b202520812a9a4
sya-ri <sya79lua@gmail.com>
2020-10-16 23:34:08 +0900

    グループ取得系の処理を削除

--- src/main/kotlin/com/gitlab/grcc/commit/graph/http/ApiEndPoint.kt
+++ src/main/kotlin/com/gitlab/grcc/commit/graph/http/ApiEndPoint.kt
@@ -4,14 +4,6 @@ import io.ktor.http.*
 
 sealed class ApiEndPoint(val method: HttpMethod, val path: String) {
     /**
-     * https://docs.gitlab.com/ee/api/groups.html#list-a-groups-subgroups
-     * ```
-     * GET /groups/:id/subgroups
-     * ```
-     */
-    class GetSubGroup(id: String): ApiEndPoint(HttpMethod.Get, "/groups/$id/subgroups")
-
-    /**
      * https://docs.gitlab.com/ee/api/groups.html#list-a-groups-projects
      * ```
      * GET /groups/:id/projects

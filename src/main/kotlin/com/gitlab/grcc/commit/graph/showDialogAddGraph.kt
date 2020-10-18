c873d3e3029d9ad89bddb96d7c4a959a85b52aec
sya-ri <sya79lua@gmail.com>
2020-10-18 16:44:22 +0900

    GraphManager.kt の宣言を showDialogAddGraph.kt に移動

--- src/main/kotlin/com/gitlab/grcc/commit/graph/showDialogAddGraph.kt
+++ src/main/kotlin/com/gitlab/grcc/commit/graph/showDialogAddGraph.kt
@@ -1,9 +1,12 @@
 package com.gitlab.grcc.commit.graph
 
-import com.gitlab.grcc.commit.graph.GraphManager.addGraphFromGroup
-import com.gitlab.grcc.commit.graph.GraphManager.addGraphFromProject
+import com.gitlab.grcc.commit.graph.gitlab.Commit.Companion.compress
 import com.gitlab.grcc.commit.graph.gitlab.Project
+import com.gitlab.grcc.commit.graph.gitlab.getAllCommits
+import com.gitlab.grcc.commit.graph.gitlab.getAllProject
 import com.gitlab.grcc.commit.graph.http.GitLabApiClient
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 import org.jfree.data.time.TimeSeries
 import org.jfree.data.time.TimeSeriesCollection
 import java.awt.Rectangle
@@ -61,5 +64,40 @@ fun showDialogAddGraph(frame: JFrame, data: TimeSeriesCollection, client: GitLab
             }
         })
         isVisible = true // ウィンドウを表示
+    }
+}
+
+@ExperimentalStdlibApi
+private fun GitLabApiClient.addGraphFromGroup(data: TimeSeriesCollection, name: String, groupId: String, onSuccess: () -> Unit, onFailure: () -> Unit) {
+    GlobalScope.launch {
+        // プロジェクトの取得
+        val projects = getAllProject(groupId) ?: return@launch onFailure.invoke()
+
+        // グラフに反映
+        addGraphFromProject(data, name, projects, onSuccess, onFailure)
+    }
+}
+
+@ExperimentalStdlibApi
+private fun GitLabApiClient.addGraphFromProject(data: TimeSeriesCollection, name: String, project: Project, onSuccess: () -> Unit, onFailure: () -> Unit) {
+    addGraphFromProject(data, name, setOf(project), onSuccess, onFailure)
+}
+
+@ExperimentalStdlibApi
+private fun GitLabApiClient.addGraphFromProject(data: TimeSeriesCollection, name: String, projects: Set<Project>, onSuccess: () -> Unit, onFailure: () -> Unit) {
+    GlobalScope.launch {
+        data.addSeries(TimeSeries(name).apply {
+            // コミットの取得
+            val commits = getAllCommits(projects) ?: return@launch onFailure.invoke()
+
+            // コミットをグラフに反映
+            val compressDates = commits.compress()
+            var sumCommit = 0
+            compressDates.forEach { (day, commit) ->
+                sumCommit += commit
+                add(day, sumCommit.toDouble())
+            }
+        })
+        onSuccess.invoke()
     }
 }

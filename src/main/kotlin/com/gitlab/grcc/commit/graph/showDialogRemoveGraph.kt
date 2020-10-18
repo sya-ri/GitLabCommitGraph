80c53c3ea82353ed522c9cc1ca7a5da21ebd005b
sya-ri <sya79lua@gmail.com>
2020-10-18 23:42:45 +0900

    削除画面で表示しきれない要素があるバグを修正

--- src/main/kotlin/com/gitlab/grcc/commit/graph/showDialogRemoveGraph.kt
+++ src/main/kotlin/com/gitlab/grcc/commit/graph/showDialogRemoveGraph.kt
@@ -1,22 +1,27 @@
 package com.gitlab.grcc.commit.graph
 
 import com.gitlab.grcc.commit.graph.api.graph.GraphData
+import java.awt.Dimension
 import java.awt.Rectangle
 import javax.swing.JButton
 import javax.swing.JDialog
 import javax.swing.JFrame
 import javax.swing.JPanel
+import javax.swing.JScrollPane
 import javax.swing.JTable
 
 @ExperimentalStdlibApi
 fun showDialogRemoveGraph(frame: JFrame, data: GraphData) {
     JDialog(frame).apply {
-        bounds = Rectangle(450, 130) // ウィンドウサイズを指定
+        bounds = Rectangle(450, 420) // ウィンドウサイズを指定
         isResizable = false // サイズ変更を無効化
         setLocationRelativeTo(null) // ウィンドウを中心に配置
         add(JPanel().apply {
             val tableModel = GraphData.TableModel(data)
-            val table = add(JTable(tableModel)) as JTable
+            val table = JTable(tableModel)
+            add(JScrollPane(table).apply {
+                preferredSize = Dimension(350, 350)
+            })
             add(JButton("選択したプロジェクトを削除").apply {
                 addActionListener {
                     while (table.selectedRow != -1) {
